e80b36e77f41c9a7b8f48942420faa2adbc1d007
daniel lukasik <daniel.lukasik10@gmail.com>
2019-10-19 21:44:35 +0200

    added first variable

---
+++ js/script.js
@@ -0,0 +1,13 @@
+printMessage("Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!");
+
+let computerMove = `kamień`;
+
+printMessage(
+	"Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!"
+);
+
+let playerMove = `papier`;
+
+printMessage(
+	"Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!"
+);
