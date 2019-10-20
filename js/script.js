f4699d4cdf017fb8375e0c8cbeb955ee4795a388
daniel lukasik <daniel.lukasik10@gmail.com>
2019-10-20 11:42:50 +0200

    added game rules to script.js

--- js/script.js
+++ js/script.js
@@ -29,3 +29,19 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+	printMessage('Ty wygrywasz!');
+  } else if( computerMove == 'papier' && playerMove == 'nożyce'){
+	printMessage('Ty wygrywasz!');
+  } else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+	printMessage('Ty wygrywasz!');
+  } else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+	  printMessage('Komputer wygrywa!');
+  } else if(computerMove == 'nożyce' && playerMove == 'papier'){
+	  printMessage('Komputer wygrywa!');
+  } else if(computerMove == 'papier' && playerMove == 'kamień'){
+	  printMessage('Komputer wygrywa!');
+  } else(computerMove == playerMove){
+	  printMessage('remis');
+  }
